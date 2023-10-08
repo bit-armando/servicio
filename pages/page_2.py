a2a679fc97be8596453aadd5808c83e5d816951b
Armando Rosales <refill.1509@gmail.com>
2023-10-08 14:54:38 -0500

    Termino de la selecion de archivos en una carpeta

--- pages/page_2.py
+++ pages/page_2.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import os
+import pandas as pd
 
 st.markdown("# Dir 📁")
 st.sidebar.markdown("# Dir 📁")
@@ -8,9 +9,30 @@ st.sidebar.markdown("# Dir 📁")
 carpeta_data = './data'
 nombres_archivos = os.listdir(carpeta_data)
 
+archivos_seleccionados = []
 for nombre_archivo in nombres_archivos:
-  agree = st.checkbox(nombre_archivo)
+    seleccionado = st.checkbox(nombre_archivo)
+    if seleccionado:
+        archivos_seleccionados.append(nombre_archivo)
 
 
-if agree:
-  st.write('Great!')
+if st.button("Combinar", type="primary"):
+    i = len(archivos_seleccionados)
+    if i == 0 or i == 1:
+        st.write("Agrega mas de un archivo para combinar")
+    else:
+        dataframes = []
+        for file in archivos_seleccionados:
+            df = pd.read_csv('./data/'+file, encoding='utf-8')
+            dataframes.append(df)
+
+        resultado = pd.concat(dataframes, ignore_index=True)
+
+        st.download_button(
+            label="Descargar",
+            data=resultado.to_csv(index=False, encoding='utf-8'),
+            file_name='resultado.csv',
+            mime='text/csv'
+        )
+
+        st.write('DataFrames combinados')
